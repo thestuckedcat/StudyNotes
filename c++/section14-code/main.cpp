0aa4a5be8aef8cd938a52157e5346f02ad6ee33a
StuckedCat <529853411@qq.com>
2023-10-23 22:08:47 +0800

    添加了重载的代码

---
+++ c++/section14-code/main.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include<vector>
+//#include"minimum_length_subarray.h"
+#include"Mystring.h"
+#define _CRT_SECURE_NO_WARNINGS
+
+using namespace std;
+
+//int main() {
+//	vector<int> test_array_for_minimum_length_subarray{2, 3, 1, 2, 4, 3};
+//	int target_for_minimum_length_subarray{ 7 };
+//
+//	Solution temp;
+//	cout << temp.minSubArrayLen(target_for_minimum_length_subarray, test_array_for_minimum_length_subarray);
+//
+//}
+
+
+int main() {
+
+    cout << boolalpha << endl;
+    Mystring a{ "frank" };
+    Mystring b{ "frank" };
+    cout << (a == b) << endl;         // true
+    cout << (a != b) << endl;           // false
+
+    b = "george";
+    cout << (a == b) << endl;         // false
+    cout << (a != b) << endl;          // true
+    cout << (a < b) << endl;         // true
+    cout << (a > b) << endl;          // false
+
+    Mystring s1{ "FRANK" };
+    s1 = -s1;
+    cout << s1 << endl;             // frank              
+
+    s1 = s1 + "*****";
+    cout << s1 << endl;             // frank*****       
+
+    s1 += "-----";                      // frank*****-----
+    cout << s1 << endl;
+
+    Mystring s2{ "12345" };
+    s1 = s2 * 3;
+    cout << s1 << endl;           // 123451234512345
+
+    Mystring s3{ "abcdef" };
+    s3 *= 5;
+    cout << s3 << endl;             // abcdefabcdefabcdefabcdefabcdef
+
+    Mystring repeat_string;
+    int repeat_times;
+    cout << "Enter a string to repeat: " << endl;
+    cin >> repeat_string;
+    cout << "How many times would you like it repeated? " << endl;
+    cin >> repeat_times;
+    repeat_string *= repeat_times;
+    cout << "The resulting string is: " << repeat_string << endl;
+
+    cout << (repeat_string * 12) << endl;
+
+    repeat_string = "RepeatMe";
+    cout << (repeat_string + repeat_string + repeat_string) << endl;
+
+    repeat_string += (repeat_string * 3);
+    cout << repeat_string << endl;
+
+    repeat_string = "RepeatMe";
+    repeat_string += (repeat_string + repeat_string + repeat_string);
+
+
+    Mystring s = "Frank";
+    ++s;
+    cout << s << endl;                  // FRANK
+
+    s = -s;
+    cout << s << endl;                  // frank
+    Mystring result;
+    result = ++s;
+    cout << s << endl;                  // FRANK
+    cout << result << endl;           // FRANK
+
+    s = "Frank";
+    s++;
+    cout << s << endl;                  // FRANK
+
+    s = -s;
+    cout << s << endl;                  // frank
+    result = s++;
+    cout << s << endl;                  // FRANK
+    cout << result << endl;           // frank
+    return 0;
+}

172574c6eff655e72d3cd2b82facf36c89e190e4
StuckedCat <529853411@qq.com>
2023-12-18 22:19:31 +0800

    tag

--- c++/Parallism/Atomic_example.h
+++ c++/Parallism/Atomic_example.h
@@ -4,6 +4,8 @@
 # include<thread>
 # include<atomic>
 # include<cassert>
+# include<memory>
+
 
 namespace compare_exchange_example {
 	void run(){
@@ -136,5 +138,280 @@ namespace memory_model_example {
 		assert(z != 0);
 
 
+	}
+}
+
+
+namespace relaxed_order {
+	std::atomic<bool> x, y;
+	std::atomic<int> z;
+
+	void write_x_then_y() {
+		x.store(true, std::memory_order_relaxed);
+		y.store(true, std::memory_order_relaxed);
+
+	}
+
+	void read_y_then_x() {
+		while (!y.load(std::memory_order_relaxed)) {
+			if (x.load(std::memory_order_relaxed)) {
+				z++;
+			}
+		}
+	}
+
+	void run_code() {
+		x = false;
+		y = false;
+		z = 0;
+
+		std::thread writer_thread(write_x_then_y);
+		std::thread reader_thread(read_y_then_x);
+		
+		
+		writer_thread.join();
+		reader_thread.join();
+
+		assert(z != 0);
+
+	}
+}
+
+
+namespace release_acquire_1 {
+	std::atomic<bool> x, y;
+	std::atomic<int> z;
+
+	void write_x_then_y() {
+		x.store(true, std::memory_order_relaxed);
+		y.store(true, std::memory_order_release);
+	}
+
+	void read_y_then_x() {
+		while (!y.load(std::memory_order_acquire));
+		if (x.load(std::memory_order_relaxed)) {
+			z++;
+		}
+	}
+
+	void run_code() {
+		x = false;
+		y = false;
+		z = 0;
+
+		std::thread writer_thread(write_x_then_y);
+		std::thread reader_thread(read_y_then_x);
+
+		writer_thread.join();
+		reader_thread.join();
+
+		assert(z != 0);
+	}
+}
+
+namespace release_acquire_2 {
+	std::atomic<bool> x, y;
+	std::atomic<int> z;
+
+	void write_x(){
+		x.store(true, std::memory_order_release);
+	}
+
+	void write_y() {
+		y.store(true, std::memory_order_release);
+	}
+
+	void read_x_then_y() {
+		while (!x.load(std::memory_order_acquire));
+
+		if (y.load(std::memory_order_acquire)) {
+			z++;
+		}
+	}
+
+	void read_y_then_x() {
+		while (!y.load(std::memory_order_acquire));
+
+		if (x.load(std::memory_order_acquire)) {
+			z++;
+		}
+	}
+
+	void run() {
+		x = false;
+		y = false;
+		z = 0;
+
+		std::thread thread_a(write_x);
+		std::thread thread_b(write_y);
+		std::thread thread_c(read_x_then_y);
+		std::thread thread_d(read_y_then_x);
+
+		thread_a.join();
+		thread_b.join();
+		thread_c.join();
+		thread_d.join();
+
+		assert(z != 0);
+	}
+}
+
+
+namespace transitive_synchronization {
+	std::atomic<int> data[5];
+	std::atomic<bool> sync1(false), sync2(false);
+
+	void thread_1_func() {
+		data[0].store(42, std::memory_order_relaxed);
+		data[1].store(45, std::memory_order_relaxed);
+		data[2].store(47, std::memory_order_relaxed);
+		data[3].store(49, std::memory_order_relaxed);
+		data[4].store(56, std::memory_order_relaxed);
+		sync1.store(true, std::memory_order_release);
+
+	
+	}
+
+
+	void thread_2_func() {
+		// 为thread1,thread3施加synchronization
+		while (!sync1.load(std::memory_order_acquire));
+		sync2.store(true, std::memory_order_release);
+	}
+
+	void thread_3_func() {
+		while (!sync2.load(std::memory_order_acquire));
+		assert(data[0].load(std::memory_order_relaxed) == 42);
+		assert(data[1].load(std::memory_order_relaxed) == 45);
+		assert(data[2].load(std::memory_order_relaxed) == 47);
+		assert(data[3].load(std::memory_order_relaxed) == 49);
+		assert(data[4].load(std::memory_order_relaxed) == 56);
+	}
+
+	void run_code(){
+		std::thread thread_1(thread_1_func);
+		std::thread thread_2(thread_2_func);
+		std::thread thread_3(thread_3_func);
+
+
+		thread_1.join();
+		thread_2.join();
+		thread_3.join();
+
+	}
+}
+
+
+namespace date_dependency_synchronization {
+	struct X {
+		int i;
+		std::string s;
+	};
+
+	std::atomic<X*>p;
+	std::atomic<int> a;
+
+	void create_x() {
+		X* x = new X;
+		x->i = 42;
+		x->s = "hello";
+
+		a.store(20, std::memory_order_relaxed);
+		p.store(x, std::memory_order_release);
+
+	}
+
+	void use_x() {
+		X* x;
+		while (!(x == p.load(std::memory_order_consume)));//synchronize only for p, no guarantee for a,当然如果用acquire，鉴于他们之间有happens-before的关系，这个时候是能保证a已经被存储的
+		assert(x->i == 42);
+		assert(x->s == "hello");
+		assert(a.load(std::memory_order_relaxed) == 20);
+	}
+
+	void run_code() {
+		std::thread thread_create(create_x);
+		std::thread thread_use(use_x);
+
+
+		thread_create.join();
+		thread_use.join();
+	}
+
+}
+
+# include <queue>
+# include<chrono>
+
+namespace Release_sequence {
+	std::atomic<int> count;
+	std::queue<int> data_queue;
+
+	int max_cout = 20;
+	void writer_queue() {
+		for (size_t i = 0; i < 20; i++) {
+			data_queue.push(i);
+		}
+		count.store(20, std::memory_order_release);
+	}
+
+	void reader_queue() {
+		while (true) {
+			int item_index = 0;
+			if (!(item_index = count.fetch_sub(1, std::memory_order_acquire) <= 0)) {//fetch sub是一个read modify write operation
+				//wait for items
+				std::this_thread::sleep_for(std::chrono::milliseconds(500));
+				continue;
+			}
+			//process the item
+		}
+	}
+
+	void run_code(){
+		std::thread writer_thread(writer_queue);
+		std::thread reader_thread1(reader_queue);
+		std::thread reader_thread2(reader_queue);
+
+		writer_thread.join();
+		reader_thread1.join();
+		reader_thread2.join();
+	}
+
+}
+
+
+namespace my_spinlock_mutex {
+	class spinlock_mutex {
+		std::atomic_flag flag = ATOMIC_FLAG_INIT;
+
+	public:
+		spinlock_mutex() {}
+
+		void lock() {
+			while (flag.test_and_set(std::memory_order_acquire));
+		}
+
+		void unlock() {
+			flag.clear(std::memory_order_release);
+		}
+	};
+
+
+
+	//演示如何使用
+	spinlock_mutex mutex;
+	void func() {
+		std::lock_guard<spinlock_mutex> lg(mutex);//any lock has both lock and unlock function can be used with guard object
+		std::cout << std::this_thread::get_id() << " hello " << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+	}
+
+	void run_code() {
+		std::thread thread1(func);
+		std::thread thread2(func);
+
+
+		thread1.join();
+		thread2.join();
 	}
 }

87eb4313f8c5a6579dc0dc46a071e8984c661eea
StuckedCat <529853411@qq.com>
2024-07-30 15:42:01 +0800

    Add tutorials about cmake/git submodule

--- Cmake/cmake_4_1/Textsome/testadd.cpp
+++ Cmake/cmake_4_1/Textsome/testadd.cpp
@@ -3,8 +3,36 @@
 
 #include"adder.h"
 #include<iostream>
+// 因为已经与include文件夹target include了，所以可以直接这样写
+#include<GLFW/glfw3.h>
 int main(){
     std::cout <<  MATH::add(2,3);
+
+
+
+    GLFWwindow *window;
+
+    if(!glfwInit()){
+        fprintf(stderr, "Failed to initalize GLFW\n");
+        exit(EXIT_FAILURE);
+    }
+
+    window = glfwCreateWindow(300, 300, "Gears", NULL, NULL);
+
+    if(!window){
+        fprintf(stderr, "Failed to open GLFW window\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    while(!glfwWindowShouldClose(window)){
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+
     return 0;
 }
 
---
+++ Cmake/cmake_4_1/build/external/glfw/cmake_uninstall.cmake
@@ -0,0 +1,29 @@
+
+if (NOT EXISTS "D:/Github-storage/StudyNotes/Cmake/cmake_4_1/build/external/glfw/install_manifest.txt")
+    message(FATAL_ERROR "Cannot find install manifest: \"D:/Github-storage/StudyNotes/Cmake/cmake_4_1/build/external/glfw/install_manifest.txt\"")
+endif()
+
+file(READ "D:/Github-storage/StudyNotes/Cmake/cmake_4_1/build/external/glfw/install_manifest.txt" files)
+string(REGEX REPLACE "\n" ";" files "${files}")
+
+foreach (file ${files})
+  message(STATUS "Uninstalling \"$ENV{DESTDIR}${file}\"")
+  if (EXISTS "$ENV{DESTDIR}${file}")
+    exec_program("C:/mingw64/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+                 OUTPUT_VARIABLE rm_out
+                 RETURN_VALUE rm_retval)
+    if (NOT "${rm_retval}" STREQUAL 0)
+      MESSAGE(FATAL_ERROR "Problem when removing \"$ENV{DESTDIR}${file}\"")
+    endif()
+  elseif (IS_SYMLINK "$ENV{DESTDIR}${file}")
+    EXEC_PROGRAM("C:/mingw64/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+                 OUTPUT_VARIABLE rm_out
+                 RETURN_VALUE rm_retval)
+    if (NOT "${rm_retval}" STREQUAL 0)
+      message(FATAL_ERROR "Problem when removing symlink \"$ENV{DESTDIR}${file}\"")
+    endif()
+  else()
+    message(STATUS "File \"$ENV{DESTDIR}${file}\" does not exist.")
+  endif()
+endforeach()
+
